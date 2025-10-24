b4a7152a0ec2df1bd68da871840ce782a688f6ec
Grace-ggg2x <graceg0021@gmail.com>
2025-10-23 18:36:38 -0700

    1023

--- excercises/ex04.js
+++ excercises/ex04.js
@@ -1,16 +1,34 @@
 let count = 0;
-let colors = ["Orchid", "Coral", "HotPink", "Plum"];
+var colors = ["Orchid", "Coral", "HotPink", "Plum"];
 
 $("#needy-button").click(function() {
-   count = count + 1;
+   
    $("#needy-button").html("You clicked me " + count + " times");
 
    $("#needy-button").html("Clicks: " + count + " Color: " + colors[count] );
 
    $("body").css("background-color", colors[count])
+
+   count = count + 1;
+
+   if (count == 4) 
+      {
+         count = 0;
+      }
 });
 
 let phrase = "hey";
 console.log(phrase);
 
 $("#cat").addClass("fluffy").fadeIn(500).text("Meow!");
+
+
+let Colors = ["Red", "Orange", "Yellow", "Green"];
+let Count = 0;
+$("#needy-button").hover(function(){
+   $("body").css("background-color", Colors[Count]);
+   Count = (Count + 1);
+   if (Count == 4){
+      Count = 0;
+   };
+});
