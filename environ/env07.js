6759fd15acbccda64b192f0750aefbf9da3e144b
Grace-ggg2x <xgu58@ucsc.edu>
2025-11-20 19:58:20 -0800

    1120

---
+++ environ/env07.js
@@ -0,0 +1,315 @@
+let SomeCommonlivingcreature = ["Lizards", "Slugcats", "Vulture", "Scavengers", "Noodleflies", "Squidcada", "Centipede", "Dropwig"];
+
+let CanSurvivorSlugcatsEatThis = true;
+
+let PlayableTypesOfSlugcats = 10;
+
+//SURVIVORS//
+let SurvivorSlugcats = {
+    specie: "Slugcats",
+    ifspecialability: false,
+    specialability: ["None"],
+    cantheyeatmeat: false,
+    availableweapon: ["Rocks", "Spears", "Explosive Spears"]
+};
+
+//let IntroOfSurvivors;//
+IntroOfSurvivors = "<p>Introduction of <strong>Survivor</strong> Slugcats:</p>";
+IntroOfSurvivors = IntroOfSurvivors + "<p>Specie: <em>" + SurvivorSlugcats.specie + "</em></p>";
+IntroOfSurvivors = IntroOfSurvivors + "<p>Special things or abilities: <em>" + SurvivorSlugcats.specialability[0] + "</em></p>";
+IntroOfSurvivors = IntroOfSurvivors + "<p>Weapons they can use: <em>" + SurvivorSlugcats.availableweapon[0];
+IntroOfSurvivors = IntroOfSurvivors + ", " + SurvivorSlugcats.availableweapon[1];
+IntroOfSurvivors = IntroOfSurvivors + ", " + SurvivorSlugcats.availableweapon[2] + "</em></p>";
+
+//$("#IntroOfSurvivors").html(IntroOfSurvivors);
+
+//document.getElementById('IntroOfSurvivors').style.color = 'white';
+//document.getElementById('IntroOfSurvivors').style.backgroundColor = 'black';
+
+
+
+//SURVIVOR PICS//
+let SurvivorSleepScreenLayerOriginalHeight = 20;
+$("#ClickToZoomIn").click(function()
+{
+    SurvivorSleepScreenLayerOriginalHeight = SurvivorSleepScreenLayerOriginalHeight + 10;
+
+    $("#SurvivorSleepScreenLayer").css("height", SurvivorSleepScreenLayerOriginalHeight)
+
+});
+
+
+let SurvivorSleepScreenLayerOriginalHeight2 = 20;
+$("#ClickToZoomIn2").click(function()
+{
+    SurvivorSleepScreenLayerOriginalHeight2 = SurvivorSleepScreenLayerOriginalHeight2 + 10;
+
+    $("#SurvivorSleepScreenLayer2").css("height", SurvivorSleepScreenLayerOriginalHeight2)
+
+});
+
+
+
+
+//HUNTERS//
+let HuntersSlugcats = {
+    specie: "Slugcats",
+    ifspecialability: true,
+    specialability: ["6 food pips are required", "a Spear can be on their back"],
+    cantheyeatmeat: true,
+    availableweapon: ["Rocks", "Spears", "Explosive Spears"]
+};
+
+//let IntroOfhunters;//
+IntroOfHunters = "<p>Introduction of <strong>Hunter</strong> Slugcats:</p>";
+IntroOfHunters = IntroOfHunters + "<p>Specie: <em>" + HuntersSlugcats.specie + "</em></p>";
+IntroOfHunters = IntroOfHunters + "<p>Special things or abilities: <em>" + HuntersSlugcats.specialability[0] + ", " + HuntersSlugcats.specialability[1] + "</em></p>";
+IntroOfHunters = IntroOfHunters + "<p>Weapons they can use: <em>" + HuntersSlugcats.availableweapon[0];
+IntroOfHunters = IntroOfHunters + ", " + HuntersSlugcats.availableweapon[1];
+IntroOfHunters = IntroOfHunters + ", " + HuntersSlugcats.availableweapon[2] + "</em></p>";
+
+
+
+//SAINT//
+let SaintSlugcats = {
+    specie: "Slugcats",
+    ifspecialability: true,
+    specialability: ["grapple and swing from walls and ceilings with their long tongue", "incapable of throwing Spears"],
+    cantheyeatmeat: false,
+    availableweapon: ["Rocks"]
+};
+//let IntroOfSaint;//
+IntroOfSaint = "<p>Introduction of <strong>Saint</strong> Slugcats:</p>";
+IntroOfSaint = IntroOfSaint + "<p>Specie: <em>" + SaintSlugcats.specie + "</em></p>";
+IntroOfSaint = IntroOfSaint + "<p>Special things or abilities: <em>" + SaintSlugcats.specialability[0] + ", " + SaintSlugcats.specialability[1] + "</em></p>";
+IntroOfSaint = IntroOfSaint + "<p>Weapons they can use: <em>" + SaintSlugcats.availableweapon[0] + "</em></p>";
+
+
+let clickOfIntroCount = 0;
+
+function changeIntro() {
+    //Intro of Survior
+    if (clickOfIntroCount == 0){
+        $("#IntroOfSlugcats").html(IntroOfSurvivors);
+        document.getElementById('IntroOfSlugcats').style.color = 'white';
+        document.getElementById('IntroOfSlugcats').style.backgroundColor = 'black';
+
+        clickOfIntroCount = clickOfIntroCount + 1;
+    }
+
+    //Intro of Hunter
+    else if (clickOfIntroCount == 1){
+        $("#IntroOfSlugcats").html(IntroOfHunters);
+        document.getElementById('IntroOfSlugcats').style.color = 'red';
+        document.getElementById('IntroOfSlugcats').style.backgroundColor = 'black';
+
+        clickOfIntroCount = clickOfIntroCount + 1;
+    }
+
+    //Intro of Saint
+    else {
+        $("#IntroOfSlugcats").html(IntroOfSaint);
+        document.getElementById('IntroOfSlugcats').style.color = 'yellowgreen';
+        document.getElementById('IntroOfSlugcats').style.backgroundColor = 'black';
+
+        clickOfIntroCount = 0;
+    }
+}
+
+$("#changeIntro-button").click(function () {
+    changeIntro();
+});
+
+
+$(function ShowTheVulture(){    
+    $("#Vulture_arrive").hover(
+        function(){
+
+            const $changeText = $(".WhatsComing").first();
+
+            if ($changeText.data("origText") == undefined) {
+                $changeText.data("origText", $changeText.text());
+            }
+
+            $changeText.text("It's a VULTURE!");
+
+            if (!$(this).prev().hasClass("TempVulture")){
+                
+                $('<img class="TempVulture" src="Vulture_Stall.png" alt="hover image">')
+                .css({ 
+                    display: "block",
+                    maxWidth: "100%",
+                    margin: "0 auto 10px"
+                })
+                
+                .insertBefore(this);
+            };
+        },
+            
+        function () {
+            const $changeText = $(".WhatsComing").first();
+            const orig = $changeText.data("origText");
+            if (orig !== undefined) $changeText.text(orig);
+
+            $(this).siblings(".TempVulture").remove();
+        }
+    );
+});
+
+//Food Inter//
+var $fruitImg = null;
+var isReadyToEat = false;
+
+//Get Food//
+$(function () {
+
+
+    $("#GetBlueFruit").click(function () {
+
+        $fruitImg = $("<img>", {
+            id: "FruitFollowCursor",
+            src: "Blue_Fruit_icon.png",
+            alt: "Fruit"
+        })
+        
+        .css({
+            pointerEvents: "none",
+            position: "absolute",
+        });
+
+        $("body").append($fruitImg);
+
+        $(document).on("mousemove.fruitFollow", function (event) {
+            if (!$fruitImg) return;
+            $fruitImg.css({
+                left: event.pageX + "px",
+                top: event.pageY + "px"
+             });
+        });
+    });
+});
+
+
+//Give Food//
+$("#WaitingForFoodSur").hover(
+    function(){
+        if (!$fruitImg) return;
+
+        $(this)
+            .attr("id", "WaitingForEating")
+            .attr("src", "DangleFruit.png")
+            .css("width", "70px");
+
+        isReadyToEat = true;
+    }
+);
+
+//Eat Food//
+$("#WaitingForEating").click(
+    $(function () {
+    $(document).on("click", "#WaitingForEating", function () {
+        if (!isReadyToEat) return;
+
+        var $img = $(this);
+
+        if ($fruitImg) {
+            $fruitImg.remove();
+            $fruitImg = null;
+        }
+
+        $img
+            .attr("id", "EatingBlueFruit")
+            .attr("src", "Survivor_eat.gif")
+            .css("width", "110px");
+
+        if (!$("#ThankYou").length) {
+            $("<p>", {
+                id: "ThankYou",
+                text: "Thank you!"
+            }).insertAfter($img);
+
+            $("#ThankYou").css({
+                color: "white",
+                fontSize: "20px",
+                fontWeight: "bold",
+                textAlign: "center",
+                marginTop: "10px"
+            });
+        }
+
+        isReadyToEat = false;
+    });
+})
+);
+
+
+//make slugwalk float and bounce//
+$(function () {
+    var $slugwalk = $("#Slugwalk");
+    $slugwalk.css("position", "absolute");
+
+    var speed = 3;
+
+    var DirectionX;
+    if (Math.random() < 0.5) {
+        DirectionX = -1;
+    }
+    else {
+        DirectionX = 1;
+    };
+
+
+    var DirectionY;
+    if (Math.random() < 0.5) {
+        DirectionY = -1;
+    }
+    else {
+        DirectionY = 1;
+    };
+
+    var MaxX = $(window).width() - $slugwalk.width();
+    var MaxY = $(window).height() - $slugwalk.height();
+
+    var PositionX = Math.random() * MaxX;
+    var PositionY = Math.random() * MaxY;
+
+    $slugwalk.css({
+        left: PositionX + "px",
+        top: PositionY + "px"
+    });
+
+    function SlugwalkMove() {
+        PositionX += speed * DirectionX;
+        PositionY += speed * DirectionY;
+
+        //fit to window
+        MaxX = $(window).width() - $slugwalk.width();
+        MaxY = $(window).height() - $slugwalk.height();
+
+        if (PositionX <= 0) {
+            PositionX = 0;
+            DirectionX = -DirectionX;
+        }
+        else if (PositionX >= MaxX) {
+            PositionX = MaxX;
+            DirectionX = -DirectionX;
+        }
+
+        if (PositionY <= 0) {
+            PositionY = -0;
+            DirectionY = -DirectionY;
+        }
+        else if (PositionY >= MaxY) {
+            PositionY = MaxY;
+            DirectionY = -DirectionY;
+        }
+
+        $slugwalk.css({
+            left: PositionX + "px",
+            top: PositionY + "px"
+        });
+    };
+    
+    setInterval(SlugwalkMove, 20);
+});
+
